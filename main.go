b9413a681b847cd0bbd9bd6c0201960ea9619816
Nikita <nikitov603@gmail.com>
2021-01-12 19:38:50 +0100

    namespace is working correctly

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"syscall"
 )
 
 //docker             run image <cmd> <params>
@@ -12,14 +14,43 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	
+	case "child":
+		child()
 	default:
 		panic("bad command")
 	}
 }
 
 func run() {
-	fmt.Print("Running ", os.Args[2:])
+	fmt.Print("Running ", os.Args[2:], "\n")
+
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:] ... ) ...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr {
+		Cloneflags: syscall.CLONE_NEWUTS, 
+	}
+	cmd.Run()
+
+}
+
+func child() {
+	fmt.Print("Running ", os.Args[2:], "\n")
+
+	syscall.Sethostname([]byte("Container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:] ...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
+
 }
 
 func must(err error) {
