0fc325898716f4eddc4873fce027c16f21578825
Nikita Belenkov <nikitov603@gmail.com>
2021-02-12 13:33:04 +0000

    Process id is now correct within the container

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 //docker             run image <cmd> <params>
-// go run main.go    run       <cmd> <params>
+//go run main.go     run       <cmd> <params>
 
 func main() {
 	switch os.Args[1] {
@@ -23,7 +23,7 @@ func main() {
 }
 
 func run() {
-	fmt.Print("Running ", os.Args[2:], "\n")
+	fmt.Print("Running ", os.Args[2:], " as ", os.Getpid(), "\n")
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:] ... ) ...)
 
@@ -32,16 +32,18 @@ func run() {
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr {
-		Cloneflags: syscall.CLONE_NEWUTS, 
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 	cmd.Run()
 
 }
 
 func child() {
-	fmt.Print("Running ", os.Args[2:], "\n")
+	fmt.Print("Running ", os.Args[2:], " as ", os.Getpid(), "\n")
 
 	syscall.Sethostname([]byte("Container"))
+	syscall.Chroot("/vagrant/ubuntu-fs")
+	syscall.Chdir("/")
 
 	cmd := exec.Command(os.Args[2], os.Args[3:] ...)
 
